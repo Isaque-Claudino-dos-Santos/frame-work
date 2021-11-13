d1c6921393d43d62d91aeb9bbfaed6078a85c734
isaque <isaqueclaudino12@gmail.com>
2021-11-13 16:35:06 -0300

    configurado para rodar class

--- autoload.php
+++ autoload.php
@@ -2,7 +2,18 @@
 
 
 spl_autoload_register(
-    function ($class) {        
-            require_once $class.".php";       
+    function ($classes) {
+        $descompact = explode("\\", $classes);
+        $path = "";
+        foreach($descompact as $files) {
+            if(end($descompact) == $files) {
+                $path .= $files;
+            }else {
+                $path .= strtolower($files)."/";
+            }
+        }
+
+        require_once $path.".php";
+       
     }
 );
