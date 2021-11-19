112c833dd60c1948e0e145c513b0a82ce39f9a8e
isaque <isaqueclaudino12@gmail.com>
2021-11-19 15:21:45 -0300

    arquivo index é dinamico para testes do script

--- index.php
+++ index.php
@@ -1,26 +1,9 @@
 <?php
 
-
 require_once("./autoload.php");
-
 use App\Controllers\UserController;
 use App\Http\Router;
 
 
-//remove e criar banco de dados
-// $deleteTable = false;
-
-// if($deleteTable == false) {
-//     $db->createTable(CreateUserTable::up());
-// }else {
-//     $db->deleteTable(CreateUserTable::down());
-// }
-
-//cria a tabela
-//$db->createTable(CreateUserTable::up());
-
-
-//insere dados
-// $db->create("user", userController::register());
-
-Router::get("/",UserController::index());
+Router::get("/",(new UserController())->index());
+(new UserController())->register();
