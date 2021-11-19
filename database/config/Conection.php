066bde36fa965f31dc2ca83d1c373f6040e776bd
isaque <isaqueclaudino12@gmail.com>
2021-11-19 15:18:21 -0300

    auteração no contrutor os metudos quando sam instanciado ja funcionam trocado por this

--- database/config/Conection.php
+++ database/config/Conection.php
@@ -1,36 +1,61 @@
 <?php
 
 namespace DataBase\Config;
-use PDO;
 
-class Conection
+use PDO;
+use PDOException;
+use PHPEnv;
+
+require_once("./.ambienteVar.php");
+
+class Conection extends PHPEnv
 {
     public  $pdo;
+    public $table = 'user';
 
-    public function __construct($HOST, $DBNAME, $USER, $PASS)
+    public function __construct()
     {
-        $this->pdo = new PDO('mysql:dbname=' . $DBNAME . ';host=' . $HOST, $USER, $PASS);
+        try {
+
+            $this->pdo = new PDO(
+                'mysql:dbname=' . $this->conectionDB["dbname"] .
+                    ';host=' .  $this->conectionDB["host"] .
+                    ';port=' . $this->conectionDB["port"],
+                $this->conectionDB["user"],
+                $this->conectionDB["password"]
+            );
+        } catch (PDOException $e) {
+            echo 'MYSQL error ' . $e->getMessage();
+        }
     }
 
-    public function createTable($array)
+    public function up()
     {
+        return [];
+    }
+
+    public function createTable()
+    {
+        $array = $this->up();
         $props = "";
         foreach ($array as $key => $value) {
             if ($key != "tableName") {
                 $props = "{$props} {$key} {$value}";
             }
         };
-        $cmd = $this->pdo->prepare("create table " . $array["tableName"] . " (" . $props . ")");
+        $cmd = $this->pdo->prepare("create table " . $this->table . " (" . $props . ")");
         $cmd->execute();
     }
 
-    public function deleteTable($table)
+
+
+    public function deleteTable()
     {
-        $cmd = $this->pdo->prepare("drop table {$table}");
+        $cmd = $this->pdo->prepare("drop table {$this->table}");
         $cmd->execute();
     }
 
-    public function create($table, $array)
+    public function insert($array)
     {
         $model_key = "";
         $model_value = "";
@@ -44,21 +69,21 @@ class Conection
             }
         }
 
+        $cmd = $this->pdo->prepare("insert into {$this->table} ({$model_value}) values({$model_key});");
 
-        $cmd = $this->pdo->prepare("insert into {$table} ({$model_value}) values({$model_key});");
-
-        function addParam($cmd,$key,$value) {
-            $cmd->bindParam(":".$key,$value);
+        function addParam($cmd, $key, $value)
+        {
+            $cmd->bindParam(":" . $key, $value);
         }
-        foreach($array as $key => $value) {
-            addParam($cmd,$key,$value);
+        foreach ($array as $key => $value) {
+            addParam($cmd, $key, $value);
         }
         $cmd->execute();
     }
 
-    public function delete($table, $array)
+    public function delete($array)
     {
-        $cmd = $this->pdo->prepare("delete from " . $table . " where id = :id");
+        $cmd = $this->pdo->prepare("delete from " . $this->table . " where id = :id");
         $cmd->bindParam("id", $array["id"]);
         $cmd->execute();
     }
