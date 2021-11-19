344247cdcc1c9443def180fa175d6ba4987d61cd
isaque <isaqueclaudino12@gmail.com>
2021-11-19 15:16:11 -0300

    rotas em desenvolvimento

--- app/http/Router.php
+++ app/http/Router.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http;
-
-class Router {
-  public static function get($req,$res)
-  {
-
-    $url = $_GET[$req];
-    if($url == "/") {
-        require_once $res;
+use app\http\Request;
+class Router 
+{
+    public static function get($req,$res)
+    {
+        $url = $_SERVER["REQUEST_URI"];
+        
+        if($url == $req) {
+            require_once $res;
+        }else {
+            echo "<h1>Error 404</h1><br/><hr/><h3>NOT FOLDE</h3>";
+        }
     }
-  }
+
+    public static function post($res) {     
+            var_dump($res);
+    }
 }
