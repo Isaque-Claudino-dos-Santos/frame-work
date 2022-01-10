ebe5346970ca72d9d8f848b5e675cdf605e94baa
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-10 17:33:38 -0300

    arquivo de rotas

--- app/http/Router.php
+++ app/http/Router.php
@@ -1,21 +1,60 @@
 <?php
 
 namespace App\Http;
-use App\http\Request;
-class Router 
+
+class Router
 {
-    public static function get($req,$res)
+    private $routers = [];
+
+    public function __construct()
     {
-        $url = $_SERVER["REQUEST_URI"];
-        
-        if($url == $req) {
-            require_once $res;
-        }else {
-            echo "<h1>Error 404</h1><br/><hr/><h3>NOT FOLDE</h3>";
+        $this->uri = $_SERVER['REQUEST_URI'];
+        $this->method = $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function get($url, $controller)
+    {
+        $this->routers[$url] = $controller;
+        $uri = $this->validateUrl($url);
+        $method = $this->validateMethod('GET');
+
+        if ($method && $uri) {
+            $this->execute($url);
         }
     }
 
-    public static function post($res) {     
-            var_dump($res);
+    private function validateMethod($method)
+    {
+        switch ($method && $this->method) {
+            case 'GET':
+                return true;
+                break;
+            case 'POST':
+                return true;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    private function validateUrl($url)
+    {
+        if ($this->uri == $url) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private function execute($url)
+    {
+        $controller = $this->routers[$url];
+
+
+        $func =isset($controller[0]) &&  $controller[0] ?  $controller[0] : 'App\Http\Controllers\HomeController';
+        $action = isset($controller[1]) &&  $controller[1] ?  $controller[1] : 'index';
+        $param = isset($controller[2]) &&  $controller[2] ?  $controller[2] : null;
+        (new $func)->$action($param);
     }
 }
