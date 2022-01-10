321ec2de3a708bded97e44d2824ef9fe9f6266e4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-10 17:31:14 -0300

    subistituido por constantis

---
+++ bootstrep.exampla.php
@@ -0,0 +1,13 @@
+<?php
+
+define('APP_PROTOCOL','http://');
+define('APP_HOST','localhost');
+define('APP_POST','2022');
+define('APP_URL',APP_PROTOCOL.APP_HOST.':'.APP_PORT);
+
+define('DB_HOST','localhost');
+define('DB_DBNAME','');
+define('DB_PORT','3306');
+define('DB_USER','');
+define('DB_PASS','');
+
