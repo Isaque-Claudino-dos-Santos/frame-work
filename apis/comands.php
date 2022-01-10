32eca861952cbf227c47584d54564e94bfc5257a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-10 17:31:41 -0300

    funções do console do artisan

---
+++ apis/comands.php
@@ -0,0 +1,30 @@
+<?php
+
+function serve()
+{
+    exec("php -S " . APP_HOST . ":" . APP_POST . " -t ./public/");
+}
+
+function istanciarClass($file, $namespace)
+{
+    $file = explode(".", $file)[0];
+    $class = $namespace . "\\" . $file;
+    return new $class;
+}
+
+function migrateUp()
+{
+    $defaulDir = "database/migrations";
+
+    if (is_dir($defaulDir)) {
+        $files = scandir($defaulDir);
+        array_map(function ($file) {
+            if ($file != "." && $file != "..") {
+                $class = istanciarClass($file, "DataBase\Migrations");
+                $class->createTable();
+            }
+        }, $files);
+    } else {
+        mkdir($defaulDir);
+    }
+}
