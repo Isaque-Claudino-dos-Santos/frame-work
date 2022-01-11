e37d30cb20fd325448fa30fdb7d14c3c274a22a1
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-11 17:34:44 -0300

    html da user create

---
+++ resources/views/user/create.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+</head>
+
+<body>
+    <form method='POST'>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label for="year">Year</label>
+        <input type="number" id="year" name="year" />
+        <button>Create User</button>
+    </form>
+</body>
+
+</html>
