7833c84fa2081fe5303d8e02872a8891999571a4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-11 17:35:30 -0300

    instanciado a  controller user/create

--- routers/web.php
+++ routers/web.php
@@ -3,7 +3,10 @@
 use App\Http\Router;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 $router = new Router();
 
 $router->get('/',[HomeController::class,'index']);
+
+$router->get('/users/create',[UserController::class,'create']);
