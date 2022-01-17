fa4cb9aa03e22e48fa1758581a9dd4ebd682eb4f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-17 16:59:01 -0300

    link com a page user

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-bt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
+
 <body>
     <h1>Welcome to page <i>HOME</i></h1>
 
     <nav>
         <ul>
-            <li><a href='#'>Create User</a></li>
+            <li><a href=<?= route('user.create'); ?>>Create User</a></li>
         </ul>
     </nav>
 </body>
+
 </html>
