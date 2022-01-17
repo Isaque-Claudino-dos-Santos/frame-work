415ff827e79c5f9793f89f95142607f2611a7a1e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-17 16:57:49 -0300

    adicionado o array de apelidos da rota e metodo para adicionar apelido

--- app/http/Router.php
+++ app/http/Router.php
@@ -4,20 +4,41 @@ namespace App\Http;
 
 class Router
 {
+
     private $routers = [];
+    private $surnames = [];
+    public $current_uri = [];
+
+    public function __get($surnames)
+    {   
+        return $this->surnames;
+    }
 
     public function __construct()
     {
-        $this->uri = $_SERVER['REQUEST_URI'];
-        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->app_uri = $_SERVER['REQUEST_URI'];
+        $this->app_method = $_SERVER['REQUEST_METHOD'];
     }
 
     public function get($url, $controller)
     {
         $this->routers[$url] = $controller;
+        $this->current_uri = $url;
         $uri = $this->validateUrl($url);
         $method = $this->validateMethod('GET');
+        return $this;
+        if (!$method && !$uri) {
+            //Aqui vem um menssagen de erro
+        }
+    }
 
+    public function post($url, $controller)
+    {
+        $this->routers[$url] = $controller;
+        $this->current_uri = $url;
+        $uri = $this->validateUrl($url);
+        $method = $this->validateMethod('POST');
+        return $this;
         if (!$method && !$uri) {
             //Aqui vem um menssagen de erro
         }
@@ -25,7 +46,7 @@ class Router
 
     private function validateMethod($method)
     {
-        switch ($method && $this->method) {
+        switch ($method && $this->app_method) {
             case 'GET':
                 return true;
                 break;
@@ -40,7 +61,8 @@ class Router
 
     private function validateUrl($url)
     {
-        if ($this->uri == $url) {
+        if ($this->app_uri == $url) {
+            
             return true;
         } else {
             return false;
@@ -49,11 +71,15 @@ class Router
 
     public function execute()
     {
-        $controller = isset($this->routers[$this->uri]) ? $this->routers[$this->uri] : $this->routers['/'];
+        $controller = isset($this->routers[$this->app_uri]) ? $this->routers[$this->app_uri] : $this->routers['/'];
         
         $func = $controller[0];
         $action = $controller[1];
         $param = isset($controller[2]) ? $controller[2] : null;
         (new $func)->$action($param);
+    }
+
+    public function name($name) {
+        $this->surnames[$name] = $this->current_uri;
     }
 }
