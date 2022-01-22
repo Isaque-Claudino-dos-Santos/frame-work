8f9ef9fa165fa18de9d5124cdf0dba943882c56b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-22 14:20:32 -0300

    pagina para não  fazer requisição

--- apis/loadApis.php
+++ apis/loadApis.php
@@ -4,7 +4,7 @@
 function loadApi()
 {
     $apiPath = scandir('../apis');
-    $apiPath = array_diff($apiPath, ['..', '.', 'loadApis.php']);
+    $apiPath = array_diff($apiPath, ['..', '.', 'loadApis.php','comands.php']);
     foreach ($apiPath as $files) {
         require_once($files);
     }
