281630d217d1b0dc41ae9dacfc42d359acfb5b28
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-23 15:41:58 -0300

    funções do comando para executar as migration

--- apis/comands.php
+++ apis/comands.php
@@ -1,5 +1,8 @@
 <?php
 
+
+require_once('./autoload.php');
+
 function serve()
 {
     exec("php -S " . APP_HOST . ":" . APP_POST . " -t ./public/");
@@ -9,20 +12,27 @@ function istanciarClass($file, $namespace)
 {
     $file = explode(".", $file)[0];
     $class = $namespace . "\\" . $file;
+
     return new $class;
 }
 
-function migrateUp()
+function executeMigration($cmd)
 {
     $defaulDir = "./app/database/migrations";
+
     if (is_dir($defaulDir)) {
+
         $files = scandir($defaulDir);
-        array_map(function ($file) {
-            if ($file != "." && $file != "..") {
-                $class = istanciarClass($file, "DataBase\Migrations");
-                $class->createTable();
+        $files = array_diff($files, ['.', '..', 'Migration.php']);
+
+        foreach ($files as $file) {
+            $class = istanciarClass($file, "App\DataBase\Migrations");
+            if ($cmd == 'migrateUp') {
+                $class->up();
+            } else if ($cmd == 'migrateDown') {
+                $class->down();
             }
-        }, $files);
+        }
     } else {
         mkdir($defaulDir);
     }
