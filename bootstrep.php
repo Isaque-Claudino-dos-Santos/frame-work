b842a889b37565456e5c3b8cb67ebb0c6a72960f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-01-23 15:43:56 -0300

    add variaveis de anbiente

---
+++ bootstrep.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('APP_PROTOCOL','http://');
+define('APP_HOST','localhost');
+define('APP_POST','2022');
+define('APP_URL',APP_PROTOCOL.APP_HOST.':'.APP_POST);
+
+define('DB_HOST','localhost');
+define('DB_DBNAME','estudo');
+define('DB_PORT','3306');
+define('DB_USER','isaque');
+define('DB_PASS','113344');
